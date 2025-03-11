935a96b5e84b0818bfe7b9d11372f6b681a34663
GABRIEL BATISTA SILVA <gabriel.bsilva104@senacsp.edu.br>
2025-03-11 16:35:42 -0300

    u.c 14

---
+++ index.html/script.js
@@ -0,0 +1,11 @@
+function mudardecor(){
+    texto= document.getElementById("txt_saudacao");
+
+    if (texto.style.color == "black"){
+        texto.style.color = "blue";
+    }
+    else{
+        texto.style.color = "black"
+    }
+}
+
